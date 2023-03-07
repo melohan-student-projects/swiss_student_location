4820db51b45aaa04d6679100482c8be77fda7dc7
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-07 22:00:30 +0100

    Update file sturacture

--- swiss_student_location/src/main.tsx
+++ swiss_student_location/src/main.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "@pages/Home";
+import Layout from "@pages/layout";
+import Register from "@pages/users/Register";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+export default function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/register' element={<Register/>}/>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+const container = document.getElementById('root')
+const root = ReactDOM.createRoot(container!);
+root.render(<App/>);
+
