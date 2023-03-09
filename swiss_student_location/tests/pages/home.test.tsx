d908a0f4c1e8383624c50b8cc331183e9a5b8f4a
melohan <melodie.ohan@cpnv.ch>
2023-03-09 22:27:12 +0100

    Add tests directory with example

---
+++ swiss_student_location/tests/pages/home.test.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Home from '@pages/home';
+
+describe('Home', () => {
+    test('renders welcome text', () => {
+        render(<Home />);
+        const welcomeText = screen.getByText('Bienvenue sur Swiss Student Location !');
+        expect(welcomeText).toBeInTheDocument();
+    });
+});
