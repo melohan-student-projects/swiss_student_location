9d59d6a722068c0908cec49cd295d4bcc390b9d1
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-16 14:46:24 +0100

    Add realstate component

--- swiss_student_location/src/pages/home.tsx
+++ swiss_student_location/src/pages/home.tsx
@@ -1,14 +1,13 @@
-import {Image} from 'primereact/image';
 import RealStateApi from "../service/realstate-api";
 import {RealState} from "../model/RealState";
-import {Button} from "primereact/button";
 import {useEffect, useState} from "react";
+import Realstate from "@components/realstate";
 
 export default function Home() {
     const [realStates, setRealStates] = useState<RealState[]>([]);
 
     useEffect(() => {
-        const realStatesApi =  new RealStateApi();
+        const realStatesApi = new RealStateApi();
         realStatesApi.getRealStates().then((realStates: RealState[]) => {
             setRealStates(realStates);
         });
@@ -16,35 +15,9 @@ export default function Home() {
 
     return (
         <div>
+            <h1>Bienvenue sur Swiss Student Location !</h1>
             <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto">
-                    <div className="flex flex-wrap -m-4">
-                        {realStates.map((realState: RealState) => {
-                            const photo = realState.photos && realState.photos.length ? realState.photos[0] : "/src/assets/images/photos/apartment1_1.jpg";
-                            return (
-                                <div className="p-4 md:w-1/3" key={realState.id}>
-                                    <div
-                                        className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-                                        <Image src={`${photo}`} alt="blog"
-                                               className="lg:h-48 md:h-36 w-full object-cover object-center"/>
-                                        <div className="p-6">
-                                            <div className="flex align-items-center gap-2">
-                                                <i className="pi pi-tag"></i>
-                                                <span className="font-semi-bold">{realState.rental_properties.type}</span>
-                                            </div>
-                                            <div className="text-2xl font-bold text-900">{`${realState.address.zip} ${realState.address.canton}`}</div>
-                                            <p className="leading-relaxed mb-3">{realState.rental_properties.description}</p>
-                                            <div className="flex sm:flex-row align-items-center justify-between gap-3 sm:gap-2">
-                                                <span className="text-2xl font-semi-bold">${realState.rental_properties.rent}</span>
-                                                <Button icon="pi pi-bookmark-fill" className="p-button-rounded" disabled={realState.rental_properties.status !== 'available'}></Button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </div>
+                <Realstate realState={realStates}/>
             </section>
         </div>
     );
--- swiss_student_location/tests/pages/home.test.tsx
+++ swiss_student_location/tests/pages/home.test.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom';
+// @ts-ignore
 import Home from '@pages/home';
 
 describe('Home', () => {
     test('renders welcome text', () => {
-        render(<Home />);
+        render(<Home/>);
         const welcomeText = screen.getByText('Bienvenue sur Swiss Student Location !');
         expect(welcomeText).toBeInTheDocument();
     });
