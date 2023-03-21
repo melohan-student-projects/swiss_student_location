997dcfdb4cf97268b36f0491a1c582b9f7c86f40
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-21 08:50:54 +0100

    Add animation

--- swiss_student_location/src/components/realstate.tsx
+++ swiss_student_location/src/components/realstate.tsx
@@ -4,13 +4,12 @@ import {Image} from 'primereact/image';
 
 export default function Realstate({realState}: { realState: RealState[] }) {
     return (
-
         <div className="container px-5 py-24 mx-auto">
             <div className="flex flex-wrap -m-4">
                 {realState.map((realState: RealState) => {
                     const photo = realState.photos && realState.photos.length ? realState.photos[0] : "/src/assets/images/photos/apartment1_1.jpg";
                     return <div className="p-4 md:w-1/3" key={realState.id}>
-                        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+                        <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden hover:scale-110 hover:border-gray-800">
                             <Image src={`${photo}`} alt="blog"
                                    className="lg:h-48 md:h-36 w-full object-cover object-center"/>
                             <div className="p-6">
