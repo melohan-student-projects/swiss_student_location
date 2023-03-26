2e7df665cac7aba2f4644b35cda4518814bd4f1b
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-26 12:28:15 +0200

    Add Offer Checklist component

--- swiss_student_location/src/pages/home.tsx
+++ swiss_student_location/src/pages/home.tsx
@@ -52,12 +52,7 @@ export default function Home() {
                             <span className="vertical-align-middle">Critères déterminats</span>
                         </div>
                     }>
-                    <p className="m-0">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                        quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-                        sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
-                        Consectetur, adipisci velit, sed quia non numquam eius modi.
-                    </p>
+                    <OfferChecklistForm/>
                 </AccordionTab>
             </Accordion>
             <section className="text-gray-600 body-font">
