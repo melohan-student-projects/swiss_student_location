74d282dfa951b6d26b789d92b4ed34662e14629c
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-26 13:31:00 +0200

    Add institution dropdown component

---
+++ swiss_student_location/src/components/InstitutionDropdown.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { Dropdown, DropdownChangeParams } from 'primereact/dropdown';
+import { Institution } from "../model/Institution";
+
+export default function InstitutionDropdown() {
+    const [selectedInstitution, setSelectedInstitution] = useState<Institution | null>(null);
+    const institutions: Institution[] = [
+        { name:'CPNV', location: 'Ste-Croix' },
+        { name:'HEIG-VD', location: 'Yverdon-les-Bains' },
+        { name: 'EPFL', location: 'Lausanne' },
+        { name: 'ETH Zurich', location: 'Zurich' },
+        { name: 'University of Basel', location: 'Basel' },
+        { name: 'University of Bern', location: 'Bern' },
+        { name: 'University of Geneva', location: 'Geneva' },
+        { name: 'University of Lausanne', location: 'Lausanne' },
+        { name: 'University of Lucerne', location: 'Lucerne' },
+        { name: 'University of Neuchâtel', location: 'Neuchâtel' },
+        { name: 'University of St. Gallen', location: 'St. Gallen' },
+        { name: 'University of Zurich', location: 'Zurich' },
+        { name: 'University of Fribourg', location: 'Fribourg' },
+        { name: 'University of Lugano', location: 'Lugano' },
+    ];
+
+    const selectedInstitutionTemplate = (option: Institution, props: any) => {
+        if (option) {
+            return (
+                <div className="flex align-items-center">
+                    <div>{option.name} ({option.location})</div>
+                </div>
+            );
+        }
+
+        return <span>{props.placeholder}</span>;
+    };
+
+    const institutionOptionTemplate = (option: Institution) => {
+        return (
+            <div className="flex align-items-center">
+                <div>{option.name} ({option.location})</div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="card flex justify-content-center">
+            <Dropdown value={selectedInstitution} onChange={(e: DropdownChangeParams) => setSelectedInstitution(e.value as Institution)} options={institutions} optionLabel="name" placeholder="Select an Institution"
+                      valueTemplate={selectedInstitutionTemplate} itemTemplate={institutionOptionTemplate} className="w-full md:w-14rem" />
+        </div>
+    )
+}
--- swiss_student_location/src/pages/home.tsx
+++ swiss_student_location/src/pages/home.tsx
@@ -2,10 +2,11 @@ import RealStateApi from "../service/realstate-api";
 import {RealState} from "../model/RealState";
 import {useEffect, useState} from "react";
 import Realstate from "@components/realstate";
-import { Accordion, AccordionTab } from 'primereact/accordion';
+import {Accordion, AccordionTab} from 'primereact/accordion';
 import OfferChecklist from "@components/offerChecklist";
 import CantonDropdown from "@components/cantonDropdown";
 import CityDropdown from "@components/cityDropdown";
+import InstitutionDropdown from "@components/InstitutionDropdown";
 
 export default function Home() {
     const [realStates, setRealStates] = useState<RealState[]>([]);
@@ -26,10 +27,20 @@ export default function Home() {
                         <div className="text-gray-900 pl-5 text-2xl mb-1 rounded-lg p-3 -mb-1 font-medium title-font">
                             <span className="vertical-align-middle">Zone géographique</span>
                         </div>
-                    } >
+                    }>
                     <div className="flex flex-wrap justify-content-center gap-3">
-                        <h3 className="text-1xl pt-3">Dans le canton de ..</h3><CantonDropdown/>
-                        <h3 className="text-1xl pt-3">Dans la ville/le village de ..</h3><CityDropdown/>
+                        <div className="text-1xl pt-3 flex flex-row">
+                            <h3 className="text-1xl p-3 ">Dans le canton de .. </h3>
+                            <CantonDropdown/>
+                        </div>
+                        <div className="text-1xl pt-3 flex flex-row">
+                            <h3 className="text-1xl p-3">Dans la ville/le village de ..</h3>
+                            <CityDropdown/>
+                        </div>
+                        <div className="text-1xl pt-3 flex flex-row">
+                            <h3 className="text-1xl p-3">Dans le quartier de .. </h3>
+                            <InstitutionDropdown/>
+                        </div>
                     </div>
                 </AccordionTab>
                 <AccordionTab
@@ -39,9 +50,12 @@ export default function Home() {
                         </div>
                     }>
                     <p className="m-0">
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                        quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-                        sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+                        laudantium, totam rem aperiam, eaque ipsa
+                        quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo
+                        enim ipsam voluptatem quia voluptas
+                        sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                        voluptatem sequi nesciunt.
                         Consectetur, adipisci velit, sed quia non numquam eius modi.
                     </p>
                 </AccordionTab>
