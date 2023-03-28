bcc2e9348a8bd84d15811101fc6cbba76f356255
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-28 10:00:26 +0200

    Add realEstate filter method

--- swiss_student_location/src/service/realstate-api.ts
+++ swiss_student_location/src/service/realstate-api.ts
@@ -1,9 +1,9 @@
 export default class RealStateApi {
     public async getRealStates(): Promise<any> {
+        console.log('http://localhost:3000/realestates')
        try {
             const response = await fetch('http://localhost:3000/realestates');
-            const responseJson = await response.json();
-            return responseJson;
+           return await response.json();
         }catch (error) {
             console.error(error);
             return [];
@@ -13,12 +13,21 @@ export default class RealStateApi {
     public async getRealStateById(id: number): Promise<any> {
         try {
             const response = await fetch('http://localhost:3000/realestates/' + id);
-            const responseJson = await response.json();
-            return responseJson;
+            return await response.json();
         } catch (error) {
             console.error(error);
             return {};
         }
     }
 
+    public async getRealStatesFiltered(filterRealStates: String) {
+        console.log('http://localhost:3000/realestates/?' + filterRealStates)
+        try {
+            const response = await fetch('http://localhost:3000/realestates/?' + filterRealStates);
+            return await response.json();
+        }catch (error) {
+            console.error(error);
+            return {};
+        }
+    }
 }
