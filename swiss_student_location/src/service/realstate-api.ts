7aa7e575afa220e0469afb90e71683f317a1f75b
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-28 08:33:46 +0200

    Add try catch for Api call

--- swiss_student_location/src/service/realstate-api.ts
+++ swiss_student_location/src/service/realstate-api.ts
@@ -1,23 +1,24 @@
 export default class RealStateApi {
     public async getRealStates(): Promise<any> {
-        return fetch(' http://localhost:3000/realestates')
-            .then((response) => response.json())
-            .then((responseJson) => {
-                return responseJson;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
+       try {
+            const response = await fetch('http://localhost:3000/realestates');
+            const responseJson = await response.json();
+            return responseJson;
+        }catch (error) {
+            console.error(error);
+            return [];
+        }
+       }
 
     public async getRealStateById(id: number): Promise<any> {
-        return fetch(' http://localhost:3000/realestates/' + id)
-            .then((response) => response.json())
-            .then((responseJson) => {
-                return responseJson;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
+        try {
+            const response = await fetch('http://localhost:3000/realestates/' + id);
+            const responseJson = await response.json();
+            return responseJson;
+        } catch (error) {
+            console.error(error);
+            return {};
+        }
     }
+
 }
