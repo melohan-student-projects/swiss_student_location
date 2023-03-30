c3685204ec7b9929a7fbedc49589a919178a1051
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-30 11:43:26 +0200

    Add test for type of realstate api

---
+++ swiss_student_location/tests/type.test.tsx
@@ -0,0 +1,37 @@
+import { test } from 'vitest';
+import {expect} from "vitest";
+import RealStateApi from "../src/services/realstate-api";
+
+
+test('getTypes', async () => {
+    const api = new RealStateApi();
+    const types = await api.getTypes();
+
+    expect(types).toBeDefined();
+    expect(Array.isArray(types)).toBe(true);
+});
+//test for the type name appartement to be in the object using give when then
+test('getTypeByName', async () => {
+    const api = new RealStateApi();
+    const types = await api.getTypes();
+    const type = types.find(type => type.name === 'Appartement');
+    expect(type).toBeDefined();
+    expect(type?.name).toBe('Appartement');
+});
+
+//test for the type name Studio to be in the object using give when then
+test('getTypeByName', async () => {
+    const api = new RealStateApi();
+    const types = await api.getTypes();
+    const type = types.find(type => type.name === 'Studio');
+    expect(type).toBeDefined();
+    expect(type?.name).toBe('Studio');
+});
+//test for the type name Villa to be in the object using give when then
+test('getTypeByName', async () => {
+    const api = new RealStateApi();
+    const types = await api.getTypes();
+    const type = types.find(type => type.name === 'Villa');
+    expect(type).toBeDefined();
+    expect(type?.name).toBe('Villa');
+}
