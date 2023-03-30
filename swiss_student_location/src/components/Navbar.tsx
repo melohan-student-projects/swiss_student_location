827f16d57197d32bc849415a02d562cdf06794bd
robiel <robiel.tesfazghi@cpnv.ch>
2023-03-30 14:39:23 +0200

    Fix log svg bug

--- swiss_student_location/src/components/Navbar.tsx
+++ swiss_student_location/src/components/Navbar.tsx
@@ -13,9 +13,9 @@ export default function Navbar() {
 
             <div>
                 <Link to={'/'} className="navbar_link flex flex-row align-items-center gap-1 text white">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                          stroke="currentColor" className="w-20 h-20 text-white">
-                        <path stroke-linecap="round" stroke-linejoin="round"
+                        <path strokeLinecap="round" strokeLinejoin="round"
                               d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819"/>
                     </svg>
 
